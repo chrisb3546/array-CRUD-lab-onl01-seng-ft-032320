9aeb869b35a47f20f99d58d1cb1dff5d058a93cd
chris brady <chrisb3546@yahoo.com>
2020-03-26 17:57:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-    myList = ["one", "two", "three", "four"]
+    myList = ["am", "two", "three", "four"]
     myList[0]
 end
 
